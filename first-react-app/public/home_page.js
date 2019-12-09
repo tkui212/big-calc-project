5a62808f48dfc389fc6e5ec34d2791c8575433f2
tkui212 <tkui12345@gmail.com>
2019-12-09 14:48:37 +0200

    START OF WORKING

---
+++ first-react-app/public/home_page.js
@@ -0,0 +1,70 @@
+
+let mouse = {
+  x: null,
+  y: null
+};
+mouseElem = () => document.elementsFromPoint(mouse.x, mouse.y);
+window.onmousemove = e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+};
+for (const element of $(`.menuos`)) {
+
+  element[`enter`] = function hover() {
+    console.log("enter");
+    if ($(`#${this.id}>.contents`)[0].style.display == "none") {
+        if($(`#${this.id}`).is(':animated')){
+      $(`#${this.id}>.contents`).stop(true, true);
+      $(`#${this.id}`).stop(true, true);
+      }
+      $(`#${this.id}>.contents`)[0].Show("up", 1000);
+      $(`#${this.id}`).animate(
+        { height: `+=${$(`#${this.id}>.contents`)[0].style.height}` },
+        1000
+      );
+    }
+  };
+  element[`exit`] = function Exit() {
+    let is = false;
+    setTimeout(() => {
+      for (const el of mouseElem()) {
+        if (el.id == this.id) {
+          is = true;
+        }
+      }
+      if (!is) {
+        // $(`#${this.id}>.contents`).stop(true, true);
+        // $(`#${this.id}`).stop(true, true);
+        // $(`#${this.id}`).animate({ height: `-=${$(`#${this.id}>.contents`)[0].style.height}` },1000);
+        if($(`#${this.id}`).is(':animated')){
+            $(`#${this.id}>.contents`).stop(true, true);
+        $(`#${this.id}`).stop(true, true);
+            $(`#${this.id}>.contents`)[0].Hide("up", 1000);
+            $(`#${this.id}`).animate({ height: `-=${$(`#${this.id}>.contents`)[0].style.height}` },1000);
+        }
+        
+        else{
+        $(`#${this.id}>.contents`)[0].Hide("up", 1000);
+        $(`#${this.id}`).animate({ height: `-=${$(`#${this.id}>.contents`)[0].style.height}` },1000);
+        }
+        
+
+      }
+    }, 10);
+  };
+    $(`#${element.id}awd`)[0][`der`] = "up";
+
+    $(`#${element.id}awd`)[0][`Hide`] = function(dir, dur) {
+    $(`#${this.id}`).hide(
+      "slide",
+      {
+        direction: dir
+      },
+      dur
+    );
+  };
+  $(`#${element.id}awd`)[0][`Show`] = function(dir, dur) {
+    $(`#${this.id}`).show("slide", { direction: dir }, dur);
+  };
+  $(`#${element.id}awd`)[0].Hide("up", 1);
+}
--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './home_page.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <menu id="home_menu">
+                <login id="login" className="menuos" onmouseover="this.enter()" onmouseout="this.exit()" style={{height: "113px;"}}>
+                    Login
+                    <div id="loginawd" className="contents" style={{display:"none"}}>
+                            <input type="text" id="login_name" placeholder="username"/>
+                            <input type="password" id="login_password" placeholder="password"/>
+                          
+                            <a>don't have one? Create new account for free!</a>
+                            </div>
+                    
+                </login>
+                <logo id="logo" className="menuos" onmouseover="this.enter()" onmouseout="this.exit()">
+                    logo
+                <div id="logoawd" className="contents" style={{display:"none"}}><p>1</p><p>2</p><p>3</p><p>4</p></div></logo>
+
+                <physic id="physic" className="menuos" onmouseover="this.enter()" onmouseout="this.exit()" style={{height: "113px;"}}>
+                    Physic
+                <div id="physicawd" className="contents" style={{display:"none"}}><p>1</p><p>2</p><p>3</p><p>4</p></div></physic>
+                <geometry id="geometry" className="menuos" onmouseover="this.enter()" onmouseout="this.exit()">
+                    Geometry
+                <div id="geometryawd" className="contents" style={{display:"none"}}><p>1</p><p>2</p><p>3</p><p>4</p></div></geometry>
+                
+            </menu>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,10 +40,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacta
         </a>
       </header>
     </div>
+    
   );
 }
 
