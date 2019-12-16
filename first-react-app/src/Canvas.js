7ecb97cc250a9bc4ed888e0d663047bc203b0bbc
tkui212 <tkui12345@gmail.com>
2019-12-16 12:05:19 +0200

    tecncly its correct

--- first-react-app/src/Canvas.js
+++ first-react-app/src/Canvas.js
@@ -35,20 +35,20 @@ export default class Canvas extends Component {
       //creating the menu arcs
       this.name = name;
       this.color = color;
-      // this.x =Math.random()*50+ Math.random()*(innerWidth-100); //setting random x set-up
-      // this.y =Math.random()*50+ Math.random()*(innerHeight-100) ; //setting random y set-up
-      // this.vx = Math.random()*5;
-      // this.vy = Math.random()*5;
+      this.x =Math.random()*50+ Math.random()*(innerWidth-100); //setting random x set-up
+      this.y =Math.random()*50+ Math.random()*(innerHeight-100) ; //setting random y set-up
+      this.vx = Math.random()*5;
+      this.vy = Math.random()*5;
       if (this.name === "1") {
-        this.x = 200;
-        this.y = 300;
-        this.vx = 5;
-        this.vy = 0;
+        this.x =Math.random()*(innerWidth-100); //setting random x set-up
+        this.y =Math.random()*(innerHeight-100) ; //setting random y set-up
+        this.vx = Math.random()*5;
+        this.vy = Math.random()*5;
       } else {
-        this.x = 304;
-        this.y = 300;
-        this.vx = -5;
-        this.vy = 0;
+        this.x =Math.random()*(innerWidth-100); //setting random x set-up
+        this.y =Math.random()*(innerHeight-100) ; //setting random y set-up
+        this.vx = Math.random()*5;
+        this.vy = Math.random()*5;
       }
       console.log("create");
       this.timer = 0;
@@ -57,6 +57,13 @@ export default class Canvas extends Component {
         c.fillStyle = this.color;
         c.strokeStyle = this.color;
         c.arc(this.x, this.y, 50, 0, 360);
+        c.fill();
+        c.strokeStyle = "black";
+        c.lineWidth = 2;
+        c.beginPath();
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.x+this.vx, this.y+this.vy);
+        c.stroke();
         c.fill();
       };
       this.calcings = function() {
@@ -88,23 +95,76 @@ export default class Canvas extends Component {
           10000;
         this.aT2 = 2 * this.a;
       };
+      this.wallQ=function(){
+        if (this.x + 50 > innerWidth&&this.vx>0 ) {
+            this.vx = -this.vx;
+            this.timer = 0;
+          }
+          if(this.x - 50 < 0&&this.vx<0){
+            this.vx = -this.vx;
+            this.timer = 0;
+          }
+          if (this.y + 50 > innerHeight &&this.vy>0){
+            this.vy = -this.vy;
+            this.timer = 0;
+          }
+          if( this.y - 50 < 0&&this.vy<0){
+            this.vy = -this.vy;
+            this.timer = 0;
+          }
+      }
+      this.collisen=function(){
+        this.vCollision = { x: this.c2.x - this.x, y: this.c2.y - this.y };
+        this.vCollisionNorm = {
+            x: this.vCollision.x / this.distance,
+            y: this.vCollision.y / this.distance
+          };
+          this.vRelativeVelocity = {
+            x: this.vx - this.c2.vx,
+            y: this.vy - this.c2.vy
+          };
+          this.speed =
+            this.vRelativeVelocity.x * this.vCollisionNorm.x +
+            this.vRelativeVelocity.y * this.vCollisionNorm.y;
+          this.vx -= this.speed * this.vCollisionNorm.x;
+          this.vy -= this.speed * this.vCollisionNorm.y;
+          this.c2.vx += this.speed * this.vCollisionNorm.x;
+          this.c2.vy += this.speed * this.vCollisionNorm.y;
+          this.con = 1 - this.con;
+          this.x += this.vx * this.con;
+          this.y += this.vy * this.con;
+          this.c2.x += this.c2.vx * this.con;
+          this.c2.y += this.c2.vy * this.con;
+      }
       this.draw();
       this.update = function() {
-        if (this.timer < 1&&this.timer!=null) {
+        if (this.name == "1") {
+            this.c2 = c1[1];
+          } else {
+            this.c2 = c1[0];
+          }
+          this.wallQ()
+          this.distance = Math.sqrt(
+            (this.c2.x - this.x) ** 2 + (this.c2.y - this.y) ** 2
+          );
+          if(this.distance<101){
+            this.collisen()
+            console.log("collisen backup")
+          }
+        if (this.timer < 2&&this.timer!=null) {
+            console.log("tests")
           //cullisen or check for next colissen
-          console.log("timer<0, need to do calcs");
+        //   console.log("timer<0, need to do calcs");
           // for(let i=0;i<2;i++){
           {
-            if (this.name == "1") {
-              this.c2 = c1[1];
-            } else {
-              this.c2 = c1[0];
-            }
+            
+            this.c2.wallQ()
             this.vCollision = { x: this.c2.x - this.x, y: this.c2.y - this.y };
             this.distance = Math.sqrt(
               (this.c2.x + this.c2.vx - this.x - this.vx) ** 2 +
                 (this.c2.y + this.c2.vy - this.y - this.vy) ** 2
             );
+            console.log(this.distance)
             if (this.distance !== 0) {
               //if not me
               console.log("if not me")
@@ -125,8 +185,8 @@ export default class Canvas extends Component {
               }
               if (this.con < 1&&this.con!=null) {
                 //if there is goning to be collisen now
-                console.log(this.con)
-                console.log("collisen will happen in less tahn a secend")
+                // console.log(this.con)
+                // console.log("collisen will happen in less tahn a secend")
                 console.log("making collisen")
                 this.x += this.vx * this.con;
                 this.y += this.vy * this.con;
@@ -138,28 +198,16 @@ export default class Canvas extends Component {
                 );
                 
                 if (this.distance < 101) {//if acsawly collisen happend
-                    console.log("collisen did happen")
+                    // console.log("collisen did happen")
                     console.log("making the rest of the collisen")
-                  this.vCollisionNorm = {
-                    x: this.vCollision.x / this.distance,
-                    y: this.vCollision.y / this.distance
-                  };
-                  this.vRelativeVelocity = {
-                    x: this.vx - this.c2.vx,
-                    y: this.vy - this.c2.vy
-                  };
-                  this.speed =
-                    this.vRelativeVelocity.x * this.vCollisionNorm.x +
-                    this.vRelativeVelocity.y * this.vCollisionNorm.y;
-                  this.vx -= this.speed * this.vCollisionNorm.x;
-                  this.vy -= this.speed * this.vCollisionNorm.y;
-                  this.c2.vx += this.speed * this.vCollisionNorm.x;
-                  this.c2.vy += this.speed * this.vCollisionNorm.y;
-                  this.con = 1 - this.con;
-                  this.x += this.vx * this.con;
-                  this.y += this.vy * this.con;
-                  this.c2.x += this.c2.vx * this.con;
-                  this.c2.y += this.c2.vy * this.con;
+                  this.collisen()
+                }
+              }
+              else{
+                this.x += this.vx;
+                this.y += this.vy;
+                if(this.timer!=null&&this.timer>0.9){
+                    this.timer-=1
                 }
               }
               this.distance = Math.sqrt(
@@ -182,33 +230,28 @@ export default class Canvas extends Component {
                   this.timer = null;
                 }
               }
-              console.log(this.timer);
+            //   console.log(this.timer);
             }
             // console.log(this)
           }
         } else {
             console.log("normal movemant")
-          if (this.x + 50 > innerWidth || this.x - 50 < 0) {
-            this.vx = -this.vx;
-            this.timer = 0;
-          }
-          if (this.y + 50 > innerHeight || this.y - 50 < 0) {
-            this.vy = -this.vy;
-            this.timer = 0;
-          }
-          console.log(`this before movemant`);
-          console.log(this);
+          
+        //   console.log(`this before movemant`);
+        //   console.log(this);
           this.x += this.vx;
           this.y += this.vy;
-          console.log(`this after movemant`);
-          console.log(this);
           if(this.timer!=null&&this.timer>0.9){
               this.timer-=1
           }
         }
         this.draw();
-        console.log(this.timer);
-        console.log(c1);
+        // console.log(this.timer);
+        // console.log(c1);
+        return new Promise(resolve =>{
+            setTimeout(() => {
+              resolve("a");
+            }, 10);})
       };
     }
     let c1 = [new create("blue", "1"), new create("red", "2")];
@@ -216,25 +259,45 @@ export default class Canvas extends Component {
     //     this.c2= new create("red" ,`hi${i}`);
     //  }
 
-    function animation() {
+  async function animation() {
       c.clearRect(0, 0, innerWidth, innerHeight);
       // requestAnimationFrame(animation);
-      console.log(c1[0]);
-      console.log(c1[1]);
+    //   console.log(c1[0]);
+    //   console.log(c1[1]);
       // c1.forEach((ele)=>{ele.update();})
-      c1[0].update();
-      c1[1].update();
-      console.log(c1[0]);
-      console.log(c1[1]);
+      await c1[0].update();
+      await c1[1].update();
+      console.log(c1);
+    //   let distance = Math.sqrt(
+    //     (c1[0].x - c1[1].x) ** 2 +
+    //       (c1[0].y - c1[1].y) ** 2
+    //   );
+    //   console.log(distance)
+    //   if(distance<100){
+    //     clearInterval(anil)
+
+    //   }
+
     }
-    canvass["ani"] = function awfddd() {
-      setTimeout(() => {
+    var anil=setInterval(() => {
         animation();
-      }, 10);
+    }, 10);
+    canvass["ani"] = function awfddd() {
+        animation()
     };
     canvass["con"] = function awdddd() {
       console.log(c1);
     };
+    
+    canvass["anistart"] = function awfddd() {
+        anil=setInterval(() => {
+            animation();
+        }, 10);
+        
+      };
+      canvass["anistop"] = function awdddd() {
+        clearInterval(anil)
+      };
     canvass["c1"] = c1;
     console.log(this);
   }
