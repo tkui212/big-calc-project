b25862d0c9943ba18319f1fe1794b6bd6b2bca52
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 00:34:07 +0200

    you forgot the remove time 1

--- first-react-app/src/Canvas.js
+++ first-react-app/src/Canvas.js
@@ -26,8 +26,6 @@ export default class Canvas extends Component {
     function log(text) {
       console.log(JSON.parse(stringify(text)))
       }
-    console.log(this);
-    console.log("mount");
     var svg = document.querySelector("#svg");
 
     // svg.width = window.innerWidth;
@@ -46,33 +44,44 @@ export default class Canvas extends Component {
       this.collider = [];
         this.combine=[];
         this.block=false;
-      this.x =Math.random()*(innerWidth-100); //setting random x set-up
-        this.y =Math.random()*(innerHeight-100) ; //setting random y set-up
-        this.vx = Math.random()*10;
-        this.vy = Math.random()*10;
-      // if(this.name=="0"){
-      //   this.x =100
-      //   this.y =200
-      //   this.vx =10
-      //   this.vy =0
-      // }
-      // else if(this.name=="1"){
-      //   this.x =1006
-      //   this.y =200
-      //   this.vx =-10
-      //   this.vy =0
-      // }
-      // else if(this.name=="2"){
-      //   this.x =500
-      //   this.y =200
-      //   this.vx =0
-      //   this.vy =10
-      // }
+      // this.x =Math.random()*(innerWidth-100); //setting random x set-up
+      //   this.y =Math.random()*(innerHeight-100) ; //setting random y set-up
+      //   this.vx = Math.random()*10;
+      //   this.vy = Math.random()*10;
+      if(this.name=="0"){
+        this.color="red"
+        this.x =250
+        this.y =300
+        this.vx =0
+        this.vy =0
+      }
+      else if(this.name=="1"){
+        this.color="blue"
+
+        this.x =131
+        this.y =300
+        this.vx =10
+        this.vy =0
+      }
+      else if(this.name=="2"){
+        this.color="green"
+
+        this.x =362
+        this.y =300
+        this.vx =-10
+        this.vy =0
+      }
+      else if(this.name=="3"){
+        this.color="yellow"
+
+        this.x =250
+        this.y =413
+        this.vx =0
+        this.vy =-10
+      }
       svg.innerHTML+= ` <circle id="${this.name}" cx="${this.x}" cy="${this.y}" r="${50}" stroke="${this.color}" stroke-width="${0}" fill="${this.color}" style="" />`
       svg.innerHTML+= ` <line id="${this.name}V" x1="${this.x}" y1="${this.y}" x2="${this.x+this.vx}" y2="${this.y+this.vy}" style="stroke:black;stroke-width:2" />`
       svg.innerHTML+= ` <line id="${this.name}ColliP" x1="${this.x}" y1="${this.y}" x2="${this.x+this.vx*0}" y2="${this.y+this.vy*0}" style="stroke:white;stroke-width:2" />`
-      
-      console.log(this);
     //   console.log(c1[0])
       this.setElements=function(){
         this.elem=document.getElementById(`${this.name}`)
@@ -81,11 +90,11 @@ export default class Canvas extends Component {
         return new Promise(resolve =>{resolve("collupdate")})
 
       }
-        
-      this.collisenUpdate=async function(){
-        console.log(this.color+" update ")
-        this.timer=[]
-        this.collider=[]
+      this.collisens=async function(){
+        console.log(this.color+" collisens ")
+        // this.timer=[]
+        // this.collider=[]
+        this.combine=[]
         let counter=0
         for(let i=0;i<c1.length;i++)
         {
@@ -101,11 +110,9 @@ export default class Canvas extends Component {
                 this.aT2;
                 let is=true
               if (this.con1 < this.con2 && this.con1 > 0) {
-                this.timer[counter] = this.con1;
-                this.collider[counter]=this.c2
+                this.combine[counter] = {T:this.con1,C:this.c2,This:this};
               } else if (this.con2 > 0) {
-                  this.timer[counter] = this.con2;
-                  this.collider[counter]=this.c2
+                this.combine[counter] = {T:this.con2,C:this.c2,This:this};
                 }
                 else{
                     is=false
@@ -113,53 +120,11 @@ export default class Canvas extends Component {
                 }
                 counter++
               }
-            //   if(this.timer==NaN){
-            //     console.error("timer cant be nan")
-            //   }
             }
-          for(let i=0;i<this.timer.length;i++){
-            //   if()
-            for(let j=0;j<this.timer.length-1;j++){
-              if(this.timer[j]>this.timer[j+1]){
-                let help=this.timer[j]
-                this.timer[j]=this.timer[j+1]
-                this.timer[j+1]=help
-                help=this.collider[j]
-                this.collider[j]=this.collider[j+1]
-                this.collider[j+1]=help
-              }
-            }
-          }
-              if(this.timer.length>0){
-                  // this.collider=this.c2
-                  console.log("")
-                  console.log()
-                  console.log(this.color+" new collider= "+this.collider[0].color)
-                  // console.log("")
-                  // console.log(this)
-                  // console.log(this.collider)
-                  // console.log(this.timer)
-                  // console.log("")
-                  console.log("this timer="+this.timer[0]+" collider timer="+this.collider[0].timer[0])
-                  console.log("")
-                  console.log("updatie to collider?="+this.collider[0].timer.length==0||this.collider[0].timer[0]!==this.timer[0]&&this.collider[0].timer[0]>this.timer[0])
-
-                  if(this.collider[0].timer.length==0||this.collider[0].timer[0]!==this.timer[0]&&this.collider[0].timer[0]>this.timer[0]){
-                      await this.collider[0].collisenUpdate();
-                  }
-                  if(this.collider[0].collider[0]!=this){
-                      for(let item of this.timer){
-                            item=-1
-                        }
-
-                  }
-              }
-        return new Promise(resolve =>{resolve("collupdate")})
+            this.sortC()
+            return this.combine
       }
       this.sortC=function(){
-          for(let i=0;i<this.timer.length;i++){
-              this.combine[i]={T:this.timer[i],C:this.collider[i]}
-          }
         this.combine=this.combine.filter((value)=>{return value.T!=0?true:false})
         this.combine=this.combine.sort((a,b)=>{return b.T>a.T?-1:1})
         for(let i=0;i<this.combine.length;i++){
@@ -174,65 +139,68 @@ export default class Canvas extends Component {
       }
       this.normalMove=async function(){
         this.x += this.vx;
-          this.y += this.vy;
-          console.log(this.timer)
-          if(this.timer.length>0&&this.timer[0]>=1){
-            console.log("e")
-            for(let i=0;i<this.timer.length;i++){
-                this.timer[i]-=1
-            }
-          }
+        this.y += this.vy;
+          // if(this.timer.length>0&&this.timer[0]>=1){
+          //   for(let i=0;i<this.timer.length;i++){
+          //       this.timer[i]-=1
+          //   }
+          // }
           return new Promise(resolve =>{resolve("end")})
       }
-      this.moveTimesCon=function(){
-        this.x += this.vx * this.con;
-        this.y += this.vy * this.con;
-        this.collider[0].x += this.collider[0].vx * this.con;
-        this.collider[0].y += this.collider[0].vy * this.con;
+      this.moveTimesCon=function(con){
+        this.x += this.vx * con;
+        this.y += this.vy * con;
+        // this.collider.x += this.collider.vx * con;
+        // this.collider.y += this.collider.vy * con;
         return new Promise(resolve =>{resolve("end")})
       }
-      this.collisenExe=async function(){
-          this.con=this.timer[0]
-          log(this)
-        await this.moveTimesCon()
-        log(this)
-        for(let i=0;i<this.timer.length;i++){
-            this.timer[i]-=this.con
-        }
-        let M=this.collider[0]
-        await this.sortC()
+      this.collisenExe=async function(collider,timer){
+
+          this.con=timer
+          this.collider=collider
+          console.log(this.color+" executing on "+this.collider.color)
+          // log(this)
+        // await this.moveTimesCon()
+        // log(this)
+        await queue.moveAll(this.con)
+        await queue.removeTime(this.con)
+        // timer-=this.con
+        // let M=this.collider
+        // await this.sortC()
         log(this)
                 this.distance = Math.sqrt(
-                  (M.x - this.x) ** 2 + (M.y - this.y) ** 2
+                  (this.collider.x - this.x) ** 2 + (this.collider.y - this.y) ** 2
                 );
                 if (this.distance < 101) {//if acsawly collisen happend
-                    this.con=1-this.con
-                    if(this.timer.length>1){
-                    while(this.timer.length>1&&this.con>this.timer[0]){
-                        let R=this.con
-                        this.con=this.timer[0]
+                    // this.con=1-this.con
+                    // if(this.timer.length>1){
+                    // while(this.timer.length>1&&this.con>this.timer[0]){
+                    //     let R=this.con
+                    //     this.con=this.timer[0]
                         await this.collisen()
-                        await this.moveTimesCon()
-                        for(let i=0;i<this.timer.length;i++){
-                            this.timer[i]-=this.con
-                        }
-                        await this.sortC()
-                        this.com=R-this.con
+                        await this.moveTimesCon(this.con)
+                        await this.collider.moveTimesCon(this.con)
+                        // for(let i=0;i<this.timer.length;i++){
+                        //     this.timer[i]-=this.con
+                        // }
+                        // await this.sortC()
+                        // this.com=R-this.con
 
-                    }
-                    }
-                    else{
-                    await this.collisen()
-                    }
+                    // }
+                    // }
+                    // else{
+                    // await this.collisen()
+                    // }
                                                     }
                 else{
                     console.error("collisen failed")
                     console.error(c1)
                     console.error(this)
                 }
-                this.timer=[]
-                this.collider[0].timer=[]
-                queue.remove(this.collider[0].name)
+                queue.remove(this.collider.name)
+                queue.remove(this.name)
+                this.draw()
+                this.collider.draw()
                 return new Promise(resolve =>{resolve("colExe")})
 
       }
@@ -304,33 +272,32 @@ export default class Canvas extends Component {
       }
       this.collisen=function(){
         this.distance = Math.sqrt(
-            (this.collider[0].x - this.x) ** 2 + (this.collider[0].y - this.y) ** 2
+            (this.collider.x - this.x) ** 2 + (this.collider.y - this.y) ** 2
           );
-        this.vCollision = { x: this.collider[0].x - this.x, y: this.collider[0].y - this.y };
+        this.vCollision = { x: this.collider.x - this.x, y: this.collider.y - this.y };
         this.vCollisionNorm = {
             x: this.vCollision.x / this.distance,
             y: this.vCollision.y / this.distance
           };
           this.vRelativeVelocity = {
-            x: this.vx - this.collider[0].vx,
-            y: this.vy - this.collider[0].vy
+            x: this.vx - this.collider.vx,
+            y: this.vy - this.collider.vy
           };
           this.speed =
             this.vRelativeVelocity.x * this.vCollisionNorm.x +
             this.vRelativeVelocity.y * this.vCollisionNorm.y;
           this.vx -= this.speed * this.vCollisionNorm.x;
           this.vy -= this.speed * this.vCollisionNorm.y;
-          this.collider[0].vx += this.speed * this.vCollisionNorm.x;
-          this.collider[0].vy += this.speed * this.vCollisionNorm.y;
+          this.collider.vx += this.speed * this.vCollisionNorm.x;
+          this.collider.vy += this.speed * this.vCollisionNorm.y;
           return new Promise(resolve =>{resolve("colli")})
       }
       this.update = async function() {
-        console.log(this.color+" up")
+        // console.log(this.color+" up ")
         if(this.wallQ()||this.timer.length==0){
             await this.collisenUpdate()
         }
         if(this.isCollisen()){
-          console.log(this.color+" executing on "+this.collider[0].color)
 
             await this.collisenExe()
         }
@@ -347,16 +314,43 @@ export default class Canvas extends Component {
     }
     var c1 = []
     // var c1 = [new create("red","0"),new create("blue","1")]
-    
     var queue={
+      TC:[],
       arr:[],
+      second:1,
+      filter:function(){
+        queue.TC=queue.TC.filter((value)=>{return value.T!=0?true:false})
+        return new Promise(resolve =>{resolve("a");})
+      },
       sort:function(){
-        queue.arr=queue.arr.sort((a,b)=>{return parseInt(b.name)>parseInt(a.name)?-1:1})
-        return new Promise(resolve =>{resolve("a");})  
+        queue.TC=queue.TC.sort((a,b)=>{return parseInt(b.T)>parseInt(a.T)?-1:1})
+        return new Promise(resolve =>{resolve("a");})
       },
         remove:function(a){
           queue.arr=queue.arr.filter((value)=>{return value.name==a?false:true})
           return new Promise(resolve =>{resolve("a");})
+        },
+        removeTime:function(num){
+          for(let ele of queue.TC){
+            ele.T-=num
+          }
+          queue.second-=num
+          queue.TC=queue.TC.filter((value)=>{return value.name==num?false:true})
+          return new Promise(resolve =>{resolve("a");})
+        },
+        getCsInSecond:function(){
+          let i=0
+          log(queue)
+          while(queue.TC[i].T<1){
+            if(queue.TC.length>i&&queue.TC[i+1].T<1){
+              i++
+            }
+            else{
+              return i
+            }
+          }
+console.error("there is no Cs in this second")
+          return null
         },
         length: function(){
           return queue.arr.length
@@ -366,35 +360,66 @@ export default class Canvas extends Component {
           await queue.sort()
           return new Promise(resolve =>{resolve("a");})
         },
+        lastCollider:async function(){
+          let i=0
+          while(queue.CT[i].T<1){
+            if(queue.CT.length<i&&queue.CT[i+1].T<1){
+              i++
+            }
+            else{
+              return queue.CT[i]
+            }
+          }
+console.error("there is no timers that are <1")
+          return null
+        },
         runQueue:async function(){
-          for(let i=0;i<queue.length();){
-            await queue.arr[i].update()
+          queue.second=1
+          if(queue.TC.length==0){//if not set
+            for(let i=0;i<queue.length();i++){
+              queue.TC=queue.TC.concat(await queue.arr[i].collisens())
+            }
+            await queue.sort()
+
+          }
+          if(queue.TC[0].T<1){//there is collisen in this second
+            let Css=queue.getCsInSecond()
+            console.log(Css)
+            for(let i=0;i<Css;i++){
+              console.log(queue.TC[i])
+              await queue.TC[i].This.collisenExe(queue.TC[i].C,queue.TC[i].T)
+            }
+            await queue.moveAll(queue.second)
+          }
+          for(let i=0;i<queue.length();){//all the others
+            await queue.arr[i].normalMove()
             await queue.remove(queue.arr[i].name)
 
           }
           return new Promise(resolve =>{resolve("end");})
+        },
+        moveAll:async function(num){
+          for(let i=0;i<c1.length;i++){
+            await c1[i].moveTimesCon(num)
+          }
+          return new Promise(resolve =>{resolve("end");})
         }
     }
-    for(let i=0;i<10;i++){
+    for(let i=0;i<4;i++){
       c1[i]=new create("red",i)
     }
     c1.forEach(element => {
       element.setElements()
     });
-    // [new create("blue", "1"), new create("red", "2"),new create("green", "3"), new create("yellow", "4")];
-    console.log(c1)
-    //  for(let i=0;i<10;i++){
-    //     this.c2= new create("red" ,`hi${i}`);
-    //  }
+
 
   async function animation() {
        await queue.set(c1);
       await queue.runQueue()
-      await c1[1].draw()
     }
     var anil=setInterval(() => {
         animation();
-    }, 50);
+    }, 1000);
     svg["ani"] = function awfddd() {
         animation()
     };
