30b69062cfd37511fcc0725bc9733ef630162c59
tkui212 <tkui12345@gmail.com>
2020-02-02 16:42:11 +0200

    fixing shit... fucks

--- Gdata.js
+++
@@ -1,8 +0,0 @@
-//they will hold the visual graph and the data behaind the graph
-//data = a,v,x,v0,x0,t
-function graph(ps,x,y,w,h){
-    this.vg=new Vg(ps,x,y,w,h)
-    // console.log(vg)
-    this.vg.fillPoints()
-    this.vg.draw()
-}
